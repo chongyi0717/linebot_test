ec5577e0627b22a1d6632f285df95d20c2fa9cee
chongyi <chongyi.chong72@gmail.com>
2021-12-25 16:15:57 +0800

    image send

--- app_core.py
+++ app_core.py
@@ -39,26 +39,11 @@ def pixabay_isch(event):
     
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
         try:
-            url = f"https://pixabay.com/images/search/{urllib.parse.urlencode({'q':event.message.text})[2:]}/"
-            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
-            
-            req = urllib.request.Request(url, headers = headers)
-            conn = urllib.request.urlopen(req)
-            
-            
-            pattern = 'img srcset="\S*\s\w*,'
-            img_list = []
-            
-            for match in re.finditer(pattern, str(conn.read())):
-                img_list.append(match.group()[12:-3])
-                
-            random_img_url = img_list[random.randint(0, len(img_list)+1)]
-            
             line_bot_api.reply_message(
                 event.reply_token,
                 ImageSendMessage(
-                    original_content_url=random_img_url,
-                    preview_image_url=random_img_url
+                    original_content_url="https://github.com/chongyi0717/wireless_project/blob/master/call_time_distribution.png",
+                    preview_image_url="https://github.com/chongyi0717/wireless_project/blob/master/call_time_distribution.png"
                 )
             )
         # 如果找不到圖，就學你說話
