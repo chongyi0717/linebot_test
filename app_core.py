ef4e4724f5a4790d1803583966f31eb1b380f9e7
chongyi <chongyi.chong72@gmail.com>
2021-12-25 18:53:26 +0800

    delete

--- app_core.py
+++ app_core.py
@@ -1,18 +1,10 @@
-from chatbase import Message
+from __future__ import unicode_literals
+import os
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-
-import requests 
-from bs4 import BeautifulSoup
-from urllib.request import urlretrieve
-import random
+import configparser
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
@@ -33,25 +25,6 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
-def movie():
-    target_url = 'https://movies.yahoo.com.tw/'
-    print('Start parsing movie ...')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')   
-    content = ""
-    for index, data in enumerate(soup.select('div.movielist_info h1 a')):
-        if index == 20:
-            return content
-        print("data：")
-        print(index)
-        print(data)        
-        title = data.text
-        link =  data['href']
-        content += '{}\n{}\n'.format(title, link)
-    return content
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
