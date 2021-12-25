c7e2a914e986ec299ed45aeddac0d9588670d219
chongyi <chongyi.chong72@gmail.com>
2021-12-25 21:39:03 +0800

    test

--- app_core.py
+++ app_core.py
@@ -4,7 +4,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler,WebhookParser
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-
+from fsm import TocMachine
 import configparser
 app = Flask(__name__)
 
