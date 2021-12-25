a582783b3aed9efaade1c75a7fe1001718b97204
chongyi <chongyi.chong72@gmail.com>
2021-12-25 21:15:52 +0800

    test

--- app_core.py
+++ app_core.py
@@ -22,84 +22,88 @@ def callback():
     body = request.get_data(as_text=True)
     #app.logger.info("Request body: " + body)
     
+    # try:
+    #     handler.handle(body, signature)
+    # except InvalidSignatureError:
+    #     abort(400)
     try:
-        handler.handle(body, signature)
+        events = parser.parse(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    #print(type(msg))
-    msg = msg.encode('utf-8')
-    flag=False
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        if event.message.text == "選單":
-            flag=True
-            line_bot_api.reply_message(  # 回復傳入的訊息文字
-                        event.reply_token,
-                        TemplateSendMessage(
-                            alt_text='Menu',
-                            template=ButtonsTemplate(
-                                title='Menu',
-                                text='今天想吃什麼樣的餐點呢？',
-                                actions=[
-                                    MessageTemplateAction(
-                                        label='中式',
-                                        text='中式'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='台式',
-                                        text='台式'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='美式',
-                                        text='美式'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='越式',
-                                        text='越式'
-                                    )
-                                ]
-                            )
-                        )
-                    )
-        elif(event.message.text=="中式"):
-            flag=True
-            line_bot_api.reply_message(  # 回復傳入的訊息文字
-                        event.reply_token,
-                        TemplateSendMessage(
-                            alt_text='Menu',
-                            template=ButtonsTemplate(
-                                title='Menu',
-                                text='今天想吃什麼樣的餐點呢？',
-                                actions=[
-                                    MessageTemplateAction(
-                                        label='中式',
-                                        text='中式'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='台式',
-                                        text='台式'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='美式',
-                                        text='美式'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='越式',
-                                        text='越式'
-                                    ),
-                                     MessageTemplateAction(
-                                        label='隨機',
-                                        text='隨機'
-                                    )
-                                ]
-                            )
-                        )
-                    )
-        if(flag!=True):
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text="抱歉，您輸入的文字還不能對應當前的stage"))
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     msg = event.message.text
+#     #print(type(msg))
+#     msg = msg.encode('utf-8')
+#     flag=False
+#     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+#         if event.message.text == "選單":
+#             flag=True
+#             line_bot_api.reply_message(  # 回復傳入的訊息文字
+#                         event.reply_token,
+#                         TemplateSendMessage(
+#                             alt_text='Menu',
+#                             template=ButtonsTemplate(
+#                                 title='Menu',
+#                                 text='今天想吃什麼樣的餐點呢？',
+#                                 actions=[
+#                                     MessageTemplateAction(
+#                                         label='中式',
+#                                         text='中式'
+#                                     ),
+#                                     MessageTemplateAction(
+#                                         label='台式',
+#                                         text='台式'
+#                                     ),
+#                                     MessageTemplateAction(
+#                                         label='美式',
+#                                         text='美式'
+#                                     ),
+#                                     MessageTemplateAction(
+#                                         label='越式',
+#                                         text='越式'
+#                                     )
+#                                 ]
+#                             )
+#                         )
+#                     )
+#         elif(event.message.text=="中式"):
+#             flag=True
+#             line_bot_api.reply_message(  # 回復傳入的訊息文字
+#                         event.reply_token,
+#                         TemplateSendMessage(
+#                             alt_text='Menu',
+#                             template=ButtonsTemplate(
+#                                 title='Menu',
+#                                 text='今天想吃什麼樣的餐點呢？',
+#                                 actions=[
+#                                     MessageTemplateAction(
+#                                         label='中式',
+#                                         text='中式'
+#                                     ),
+#                                     MessageTemplateAction(
+#                                         label='台式',
+#                                         text='台式'
+#                                     ),
+#                                     MessageTemplateAction(
+#                                         label='美式',
+#                                         text='美式'
+#                                     ),
+#                                     MessageTemplateAction(
+#                                         label='越式',
+#                                         text='越式'
+#                                     ),
+#                                      MessageTemplateAction(
+#                                         label='隨機',
+#                                         text='隨機'
+#                                     )
+#                                 ]
+#                             )
+#                         )
+#                     )
+#         if(flag!=True):
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text="抱歉，您輸入的文字還不能對應當前的stage"))
 
 # 學你說話
 # stage=["type","location"]
@@ -115,5 +119,4 @@ def handle_message(event):
 #             line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=random.randint(1,10), sticker_id=random.randint(1,10)))
 
 if __name__ == "__main__":
-    current_stage="nothing"
     app.run()
